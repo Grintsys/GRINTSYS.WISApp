db084b9e74de3f4d259d75e7c2cc594fb7987c9d
Serpel <serpel.js@gmail.com>
2018-07-31 10:12:53 -0600

    Send messages

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,14 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Sergio Peralta on 5/31/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "RCTOneSignalExtensionService.h"
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
